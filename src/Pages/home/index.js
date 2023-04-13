87140a489e80a208275da87c88f965fe7e756dec
unknown <murilorez79@hotmail.com>
2023-04-13 15:55:59 -0300

    Criação de Home

---
+++ src/Pages/home/index.js
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+
+function Home(){
+    return (
+        <Container>
+            <h1>Bem-vindo(a)</h1>
+        </Container>
+    );
+}
+
+export default Home;
---
+++ src/Pages/home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    min-width: 100vw;
+    background-color: #383838;
+`
