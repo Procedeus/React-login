fefbc07ca2981a1c13629aa7353e6550f601254e
Murilo Rezende <murilorez79@hotmail.com>
2023-05-10 12:04:33 -0300

    Refatoração do setToken

--- src/services/api.js
+++ src/services/api.js
@@ -1,29 +1,23 @@
 import axios from 'axios';
 
+function setToken(token){
+    const setToken = token;
+    localStorage.setItem('token', setToken);
+}
+
 export default class UserServices{
     constructor () {
         this.axios = axios.create({
             baseURL: 'http://localhost:3333/'
         })
     }
-
     async Login(data){
         const response = await this.axios.post('/login', data);
-        const user = response.data;
-        if(user.token){
-            localStorage.setItem('token', user.token);
-            return true;
-        }
-        return false;
+        setToken(response.data);
     }
     async Signup(data){
         const response = await this.axios.post('/signup', data);
-        const user = response.data;
-        if(user.token){
-            localStorage.setItem('token', user.token);
-            return true;
-        }
-        return false;
+        setToken(response.data);
     }
     async UserAuth() {
         const token = localStorage.getItem('token');
